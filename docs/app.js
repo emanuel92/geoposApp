8de969cd50b94b808aaa0620c6262a29615fb369
Emanuel Wille <wille.emanuel@gmail.com>
2019-06-08 10:17:56 +0200

    first commit

---
+++ docs/app.js
@@ -0,0 +1,48 @@
+// const fixerUri = 'http://data.fixer.io/api/latest?base=EUR&symbols=USD,CHF,SEK&access_key=ab065aa859974a9a0e3c93a87ffa22a9';
+const fixerUri = 'rates.json';
+
+document.querySelectorAll('select').forEach(element => {
+    element.innerHTML = `
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+        <option value="CHF">CHF</option>
+        <option value="SEK">SEK</option>
+    `;
+});
+
+document.querySelector('button').addEventListener('click', () => {
+    const inputValue = document.querySelector('[name="input-value"]').value;
+    const inputCurrency = document.querySelector('[name="input-currency"]').value;
+    const outputCurrency = document.querySelector('[name="output-currency"]').value;
+
+    convert(inputValue, inputCurrency, outputCurrency)
+        .then((outputValue) => {
+            document.querySelector('[name="output-value"]').value = outputValue;
+        });
+})
+
+function convert(inputValue, inputCurrency, outputCurrency) {
+    return new Promise((resolve, reject) => {
+        fetch(fixerUri).then(response => {
+            if(response.status == 200) {
+                return response.json();
+            } else {
+                return Promise.reject('failed to download rates');
+            }
+        }).then((data) => {
+            let rates = data['rates'];
+            rates["EUR"] = 1;
+
+            if (inputCurrency != "EUR") {
+                inputValue = inputValue / rates[inputCurrency];
+            }
+
+            resolve(inputValue * rates[outputCurrency]);
+        });     
+    });
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .catch(console.error);
+}
